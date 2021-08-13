1064c457401cb5977457be117291e93cc8bc7684
Alexandre Wolff <alexandrewlff@gmail.com>
2021-08-13 17:15:11 +0200

    Add test USDC to deployment on testnet

--- migrations/2_crowdsale.js
+++ migrations/2_crowdsale.js
@@ -1,24 +1,40 @@
 const TotemToken = artifacts.require('TotemToken');
 const TotemCrowdsale = artifacts.require('TotemCrowdsale');
+const LambdaToken = artifacts.require('LambdaToken');
 
 const wallet = '0x535Ea2751927C0Ce9086b0a7ECAfA82698Bb3958';
-const saleStart = 1628777020;
-const saleEnd = 1631369020;
+const saleStart = 0;
+const saleEnd = 2031369020;
+// const saleStart = 1628777020;
+// const saleEnd = 1631369020;
 const exchangeRate = 50;
 const referralPercentage = 2;
-const bscStableCoins = ['0x535Ea2751927C0Ce9086b0a7ECAfA82698Bb3958'];
+const bscStableCoins = [];
 
 // const computeDates
 
-module.exports = async (deployer, _, accounts) => {
+module.exports = async (deployer, network, accounts) => {
   const token = await TotemToken.deployed();
 
-  let saleStart = new Date();
-  saleStart.setDate(saleStart.getDate() + 1);
-  let saleEnd = new Date(saleStart);
-  saleEnd.setDate(saleStart.getDate() + 30);
-  saleStart = Math.floor(saleStart.getTime() / 1000);
-  saleEnd = Math.floor(saleEnd.getTime() / 1000);
+  // let saleStart = new Date();
+  // saleStart.setDate(saleStart.getDate() + 1);
+  // let saleEnd = new Date(saleStart);
+  // saleEnd.setDate(saleStart.getDate() + 30);
+  // saleStart = Math.floor(saleStart.getTime() / 1000);
+  // saleEnd = Math.floor(saleEnd.getTime() / 1000);
+
+  if (network === 'bsc_test') {
+    const testUsdc = await deployer.deploy(
+      LambdaToken,
+      'USDC',
+      'USDC',
+      new web3.utils.BN('1000000000000000000000000', 10),
+      {
+        from: accounts[0],
+      }
+    );
+    bscStableCoins.push(testUsdc.address);
+  }
 
   await deployer.deploy(
     TotemCrowdsale,
